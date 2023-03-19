47bf699b0f67c196ce294b0f5ae7973f033e74e3
Jfnrosimo <j.rosimo098.com>
2023-03-19 19:54:53 +0800

    Feat: completed startup setup for mvvm

--- ViewSwitch/App.xaml.cs
+++ ViewSwitch/App.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using ViewSwitch.MVVM.View;
+using ViewSwitch.MVVM.ViewModel;
 
 namespace ViewSwitch
 {
@@ -13,5 +10,28 @@ namespace ViewSwitch
     /// </summary>
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<MainWindow>(provider => new MainWindow
+            {
+                DataContext = provider.GetRequiredService<MainViewModel>()
+            }) ;
+            services.AddSingleton<MainViewModel>();
+            services.AddSingleton<HomeViewModel>();
+            services.AddSingleton<SettingsView>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+            base.OnStartup(e);
+        }
     }
 }
